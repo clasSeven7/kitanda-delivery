5d2852ad9fd3e62d370275bb70076566d884e6d2
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-04-29 19:56:13 -0300

    feat: Add new dependencies: django-environ and psycopg2-binary
    Adicionar novas dependências: django-environ e psycopg2-binary

--- kitanda/settings.py
+++ kitanda/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'kitanda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kitanda_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres-admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
