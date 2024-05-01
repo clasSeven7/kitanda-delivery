6066902ec5e3b1793c9e47548314fc41696cc3de
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-05-01 17:09:29 -0300

    Refactor admin.py, templates, views.py, and models.py for better code organization and readability

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Blog(models.Model):
     user = models.CharField(max_length=50)
@@ -8,7 +7,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     url = models.URLField()
-    image = models.ImageField(upload_to='blogs/images/', null=True, blank=True) 
-    
+    image = models.ImageField(upload_to='blogs/images/', null=True, blank=True)
+
     def __str__(self):
         return self.title
--- plataforma/views.py
+++ plataforma/views.py
@@ -8,4 +8,4 @@ def home_page(request):
     products = Product.objects.all()
     categories = Categorie.objects.all()
     comments = Comment.objects.all()
-    return render(request, 'plataforma/home_page.html', {'resources': resources, 'products': products, 'categories': categories, 'comments': comments})
+    return render(request, 'plataforma/home.html', {'resources': resources, 'products': products, 'categories': categories, 'comments': comments})
