91b04b331b2d7bf7c227ada11f3dad41b0bec141
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-05-05 23:40:07 -0300

    Refactor templates and views for better code organization and readability | styled all project components

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .class_views.Blog import (BlogCreateView, BlogDeleteView, BlogDetailView,
+                               BlogUpdateView)
 from .class_views.Categorie import (CategorieCreateView, CategorieDeleteView,
                                     CategorieDetailView, CategorieUpdateView)
 from .class_views.Comment import (CommentCreateView, CommentDeleteView,
@@ -8,55 +10,53 @@ from .class_views.Product import (ProductCreateView, ProductDeleteView,
                                   ProductDetailView, ProductUpdateView)
 from .class_views.Resource import (ResourceCreateView, ResourceDeleteView,
                                    ResourceDetailView, ResourceUpdateView)
-
-from .class_views.Blog import (BlogCreateView, BlogDeleteView, BlogDetailView,
-                               BlogUpdateView)
-
 from .views import home
 
-
 URLSBLOGS = [
-     path('create-blog/', BlogCreateView.as_view(), name='blog_create'),
-     path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-     path('<int:pk>/update-blog/', BlogUpdateView.as_view(), name='blog_update'),
-     path('<int:pk>/delete-blog/', BlogDeleteView.as_view(), name='blog_delete'),
-     ]
+    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:pk>/update/',
+         BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<int:pk>/delete/',
+         BlogDeleteView.as_view(), name='blog_delete'),
+]
 
 
 URLSPRODUCTS = [
-    path('create-product/', ProductCreateView.as_view(), name='product_create'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('<int:pk>/update-product/',
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/update/',
          ProductUpdateView.as_view(), name='product_update'),
-    path('<int:pk>/delete-product/',
+    path('product/<int:pk>/delete/',
          ProductDeleteView.as_view(), name='product_delete'),
 ]
 
 URLSRESOURCES = [
-    path('create-resource/', ResourceCreateView.as_view(), name='resource_create'),
-    path('<int:pk>/', ResourceDetailView.as_view(), name='resource_detail'),
-    path('<int:pk>/update-resource/',
+    path('resource/create/', ResourceCreateView.as_view(), name='resource_create'),
+    path('resource/<int:pk>/', ResourceDetailView.as_view(), name='resource_detail'),
+    path('resource/<int:pk>/update/',
          ResourceUpdateView.as_view(), name='resource_update'),
-    path('<int:pk>/delete-resource/',
+    path('resource/<int:pk>/delete/',
          ResourceDeleteView.as_view(), name='resource_delete'),
 ]
 
 URLSCOMMENTS = [
-    path('create-comment/', CommentCreateView.as_view(), name='comment_create'),
-    path('<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),
-    path('<int:pk>/update-comment/',
+    path('comment/create/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),
+    path('comment/<int:pk>/update/',
          CommentUpdateView.as_view(), name='comment_update'),
-    path('<int:pk>/delete-comment/',
+    path('comment/<int:pk>/delete/',
          CommentDeleteView.as_view(), name='comment_delete'),
 ]
 
 URLSCATEGORIES = [
-    path('create-categorie/', CategorieCreateView.as_view(),
+    path('categorie/create', CategorieCreateView.as_view(),
          name='categorie_create'),
-    path('<int:pk>/', CategorieDetailView.as_view(), name='categorie_detail'),
-    path('<int:pk>/update-categorie/',
+    path('categorie/<int:pk>/', CategorieDetailView.as_view(),
+         name='categorie_detail'),
+    path('categorie/<int:pk>/update/',
          CategorieUpdateView.as_view(), name='categorie_update'),
-    path('<int:pk>/delete-categorie/',
+    path('categorie/<int:pk>/delete/',
          CategorieDeleteView.as_view(), name='categorie_delete'),
 ]
 
